292da8d375892d377acc1820687fbfa3fd752be8
Max Rosenthal <mxrosenthal@yahoo.com>
2020-01-21 01:34:46 +0000

    added a post route so users can enter their own URL and my 'server' is updated.

--- express_server.js
+++ express_server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const PORT = 8080;
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs');
 
@@ -9,9 +11,8 @@ const urlDatabase = {
   '9sm5xK': 'http://www.google.com'
 };
 
-app.get('/urls', (req, res) => {
-  let templateVars = { urls: urlDatabase };
-  res.render('urls_index', templateVars);
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
 });
 
 app.get('/urls/:shortURL', (req, res) => {
@@ -26,6 +27,11 @@ app.get('/urls/:shortURL', (req, res) => {
   res.render('urls_show', templateVars);
 });
 
+app.get('/urls', (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render('urls_index', templateVars);
+});
+
 app.get('/hello', (req, res) => {
   res.send('<html><body>Hello <b>World</b></body></html>\n');
 });
@@ -38,6 +44,25 @@ app.get('/', (req, res) => {
   res.send('Hello!');
 });
 
+//POST
+app.post('/urls', (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  urlDatabase[generateRandomString()] = req.body.longURL;
+  console.log('urlDatabase: ', urlDatabase);
+  res.send('Ok'); // Respond with 'Ok' (we will replace this)
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+function generateRandomString() {
+  return Math.round(Math.pow(36, 6 + 1) - Math.random() * Math.pow(36, 6))
+    .toString(36)
+    .slice(1);
+}
+
+// console.log(generateRandomString());
+// console.log(generateRandomString());
+// console.log(generateRandomString());
+// console.log(generateRandomString());
