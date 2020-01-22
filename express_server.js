44f33fa89d24e793badb514ca4621f630cfdee9f
Max Rosenthal <mxrosenthal@yahoo.com>
2020-01-22 22:18:16 +0000

    login and register work. moving on to user specific features.

--- express_server.js
+++ express_server.js
@@ -9,103 +9,77 @@ app.use(cookieParser());
 
 app.set('view engine', 'ejs');
 
-const urlDatabase = {
-  // b2xVn2: 'http://www.lighthouselabs.ca',
-  // '9sm5xK': 'http://www.google.com'
-};
+const urlDatabase = {};
 
 const users = {
   user2RandomID: {
     id: 'user2RandomID',
     email: 'user2@example.com',
-    password: 'dishwasher-funk'
+    password: '123'
   }
 };
 
-// app.get('/login', (req, res) => {
-//   res.redirect('/login');
-// });
-
 app.get('/urls/new', (req, res) => {
   const userID = req.cookies['user_id'];
-  // console.log('userID: ', userID);
-
   const userObj = users[userID];
-  // console.log('userObj: ', userObj);
+
   let templateVars = {
     user: userObj,
     shortURL: req.params.shortURL,
     longURL: urlDatabase[req.params.shortURL]
   };
-  // res.render('urls_new', templateVars);
+
   res.render('urls_new', templateVars);
 });
 
 app.get('/login', (req, res) => {
-  // console.log('userID: ', userID);
-
   const userID = req.cookies['user_id'];
-  // console.log('userID: ', userID);
-
   const userObj = users[userID];
-  // console.log('userObj: ', userObj);
+
   let templateVars = {
     user: userObj,
     shortURL: req.params.shortURL,
     longURL: urlDatabase[req.params.shortURL]
   };
-  // console.log('logging in?');
-  // res.render('urls_user', templateVars);
+
   res.render('urls_login', templateVars);
 });
 
-//rendering the registration page.
 app.get('/register', (req, res) => {
-  // if (!userID) {
-  //   res.redirect('/register');
-  // }
-
   const userID = req.cookies['user_id'];
-  // console.log('userID: ', userID);
-
   const userObj = users[userID];
-  // console.log('userObj: ', userObj);
+
   let templateVars = {
     user: userObj,
     shortURL: req.params.shortURL,
     longURL: urlDatabase[req.params.shortURL]
   };
 
-  // console.log(templateVars);
-  // console.log('registering');
   res.render('urls_user', templateVars);
 });
 
 app.get('/urls/:shortURL', (req, res) => {
   const userID = req.cookies['user_id'];
-  // console.log('userID: ', userID);
   const userObj = users[userID];
-  // console.log('user: ', userObj);
+
   let templateVars = {
     user: userObj,
     shortURL: req.params.shortURL,
     longURL: urlDatabase[req.params.shortURL]
   };
+
   res.render('urls_show', templateVars);
 });
 
 app.get('/urls', (req, res) => {
   const userID = req.cookies['user_id'];
-  // console.log('userID: ', userID);
-
   const userObj = users[userID];
-  // console.log('userObj: ', userObj);
 
   let templateVars = {
     user: userObj,
     urls: urlDatabase
   };
-  // console.log(templateVars);
+
   res.render('urls_index', templateVars);
 });
 
@@ -129,61 +103,54 @@ app.post('/urls', (req, res) => {
     res.redirect('/urls');
     r;
   }
+
   urlDatabase[generateRandomString()] = req.body.longURL;
   res.redirect('/urls');
 });
 
 //request to change the longURL associated with a given shortURL
 app.post('/urls/:shortURL', (req, res) => {
-  // console.log('req: ', req.params.shortURL);
   const longURL = req.body.longURL;
-  // console.log(longURL);
   const shortURL = req.params.shortURL;
-  // console.log('Database: ', urlDatabase);
   urlDatabase[shortURL] = longURL;
-  // console.log('Database: ', urlDatabase);
   res.redirect('/urls');
 });
 
 //Delete longURL with it's shortURL key from the database.
 app.post('/urls/:shortURL/delete', (req, res) => {
   let shorty = req.params.shortURL;
-  // let URL = urlDatabase[shorty];
   delete urlDatabase[shorty];
   res.redirect('/urls');
 });
 
 //add users logged into a cookie. You can check this cookie in the CDT.
 // 1. open chrom dev tools, clear cache if full.
-// 2. submit username on tinyapp.
+// 2. submit user_id on tinyapp.
 // 3. in CDT go to 'application' at the top. then 'cookies' on the left.
 //    and the submission should be visible.
 app.post('/login', (req, res) => {
   let emailToFind = req.body.email;
-
+  let password = req.body.password;
   let userLoggingIn;
+
   for (const userID in users) {
     if (users[userID].email === emailToFind) {
       userLoggingIn = users[userID];
+    } else {
+      //email not registered. redirect to register page
+      res.send('Error 403: Email and/or password incorrect.');
     }
   }
-  console.log('userLoggingIn: ', userLoggingIn);
-  let password = req.body.password;
-  console.log(password);
+
   if (userLoggingIn.password === password) {
     res.cookie('user_id', userLoggingIn.id).redirect('/urls');
   } else {
-    //need redirect if password fails.
-    res.redirect('/login');
+    res.send('Error 403: Email and/or password incorrect.');
   }
 });
 
 //logging out
 app.post('/logout', (req, res) => {
-  console.log('POST logout');
-  console.log('req.cookie: ', req.cookie);
-  console.log('req.cookies: ', req.cookies);
-
   res.clearCookie('user_id', req.cookies['user_id']).redirect('/urls');
 });
 
@@ -203,7 +170,6 @@ app.post('/register', (req, res) => {
   users[userID]['id'] = userID;
   users[userID]['email'] = req.body.email;
   users[userID]['password'] = req.body.password;
-  console.log(users);
 
   res.cookie('user_id', userID).redirect('/urls');
 });
