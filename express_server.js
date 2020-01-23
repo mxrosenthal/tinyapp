092096f6c0918de2117c4d7aacf290cd388bf45f
Max Rosenthal <mxrosenthal@yahoo.com>
2020-01-23 23:18:13 +0000

    added images to the docs file that adequately represent the app. cleared old unused dependancies from express_server.js

--- express_server.js
+++ express_server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 const bodyParser = require('body-parser');
-// const cookieParser = require('cookie-parser');
 const bcrypt = require('bcrypt');
 const cookieSession = require('cookie-session');
 const helpers = require('./helpers');
